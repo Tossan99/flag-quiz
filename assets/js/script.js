cf232de100aeca36ff6bdb769f3c27173fad4f8f
Tossan99 <>
2023-08-07 09:44:04 +0000

    Add question structure in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,103 @@ function startGame() {
     rules.classList.add("hide");
     startButton.classList.add("hide");
     questionContainer.classList.remove("hide");
+    nextQuestion()
+}
+
+const questions = [
+    {
+        question: "flag of norway"
+        answers: [
+            {text: "Sweden", correct: false},
+            {text: "Denmark", correct: false},
+            {text: "Finland", correct: false},
+            {text: "Norway", correct: true}
+        ]
+    },
+    {
+        question: "flag of sweden"
+        answers: [
+            { text: "Sweden", correct: true },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: false }
+        ]
+    },
+    {
+        question: "flag of norway"
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: true }
+        ]
+    },
+    {
+        question: "flag of norway"
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: true }
+        ]
+    },
+    {
+        question: "flag of norway"
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: true }
+        ]
+    },
+    {
+        question: "flag of norway"
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: true }
+        ]
+    },
+    {
+        question: "flag of norway"
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: true }
+        ]
+    },
+    {
+        question: "flag of norway"
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: true }
+        ]
+    },
+    {
+        question: "flag of norway"
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: true }
+        ]
+    },
+    {
+        question: "flag of norway"
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+            { text: "Norway", correct: true }
+        ]
+    },
+
+]
+
+function nextQuestion() {
+
 }
